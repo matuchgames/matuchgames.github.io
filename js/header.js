2ef7c49393e1a081ad291a7cdb9fb3fcad1a2619
matuchgames <90116345+matuchgames@users.noreply.github.com>
2025-05-04 23:32:08 +0200

    Create header.js

---
+++ js/header.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const headerHTML = `
+        <header>
+            <a href="./" class="logo"><img src="img/logo/mg_240x40.png"></a>
+            <div class="bx bx-menu" id="menu-icon"></div>
+
+            <ul class="navbar">
+                <li><a href="games" data-after="Games">Games</a></li>
+                <li><a href="news" data-after="News">News</a></li>
+                <li><a href="support" data-after="Support">Support</a></li>
+            </ul>
+        </header>
+    `;
+
+    // Znajdź miejsce, w którym chcesz umieścić nagłówek (np. div o id "header")
+    const headerContainer = document.getElementById('header');
+    if (headerContainer) {
+        headerContainer.innerHTML = headerHTML;
+    }
+});
