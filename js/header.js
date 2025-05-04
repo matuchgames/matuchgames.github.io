c03df87b2f1efa3ffc9c0df27e3eb88740fbbfc5
matuchgames <90116345+matuchgames@users.noreply.github.com>
2025-05-05 00:07:50 +0200

    Update header.js

--- js/header.js
+++ js/header.js
@@ -12,9 +12,28 @@ document.addEventListener("DOMContentLoaded", function () {
         </header>
     `;
 
-    // Znajdź miejsce, w którym chcesz umieścić nagłówek (np. div o id "header")
     const headerContainer = document.getElementById('header');
     if (headerContainer) {
         headerContainer.innerHTML = headerHTML;
+
+        // Dodaj event listener do menu po załadowaniu nagłówka
+        let menu = document.querySelector("#menu-icon");
+        let navbar = document.querySelector(".navbar");
+
+        menu.addEventListener("click", function () {
+            navbar.classList.toggle("active");
+            if (navbar.classList.contains("active")) {
+                menu.classList.remove("bx-menu");
+                menu.classList.add("bx-x");
+            } else {
+                menu.classList.remove("bx-x");
+                menu.classList.add("bx-menu");
+            }
+        });
+
+        // Ukryj menu przy przewijaniu
+        window.onscroll = () => {
+            navbar.classList.remove("active");
+        };
     }
 });
