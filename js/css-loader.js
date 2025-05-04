121b82655a4271004f3256f9ad5541edeaa761fc
matuchgames <90116345+matuchgames@users.noreply.github.com>
2025-05-04 23:14:39 +0200

    Update css-loader.js

--- js/css-loader.js
+++ js/css-loader.js
@@ -1,5 +1,5 @@
 const version = "1.0.3"; // Zmieniasz tylko tutaj po aktualizacji CSS
 const css = document.createElement("link");
 css.rel = "stylesheet";
-css.href = "css/style.css?v=" + version;
+css.href = "/css/style.css?v=" + version;
 document.head.appendChild(css);
