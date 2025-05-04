1fe8a0b45d3ad021e0b61eb22583b0b9bb254eca
matuchgames <90116345+matuchgames@users.noreply.github.com>
2025-05-04 23:27:52 +0200

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -3,5 +3,5 @@ footer.innerHTML = `
     <a target="_blank" href="https://www.youtube.com/@MatuchGames"><img src="/img/footer-icons/yt-icon.png" /></a>
     <a target="_blank" href="https://twitter.com/MatuchGames"><img src="/img/footer-icons/x-icon.png" /></a>
     <a target="_blank" href="https://instagram.com/"><img src="/img/footer-icons/ig-icon.png" /></a>
-    <p>&copy; TEST matuchgames.com. All rights reserved.</p>
+    <p>&copy; ${year} matuchgames.com. All rights reserved.</p>
 `;
