4c090dc48412250b119834f680f6926c4a36a99a
matuchgames <90116345+matuchgames@users.noreply.github.com>
2025-05-04 23:51:51 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,23 @@
-let menu = document.querySelector("#menu-icon");
-let navbar = document.querySelector(".navbar");
+const headerContainer = document.getElementById('header');
+    if (headerContainer) {
+        headerContainer.innerHTML = headerHTML;
 
-menu.addEventListener("click", function () {
-    navbar.classList.toggle("active");
-    if (navbar.classList.contains("active")) {
-        menu.classList.remove("bx-menu");
-        menu.classList.add("bx-x");
-    } else {
-        menu.classList.remove("bx-x");
-        menu.classList.add("bx-menu");
-    }
-});
+        // Dodaj event listener do menu po załadowaniu nagłówka
+        let menu = document.querySelector("#menu-icon");
+        let navbar = document.querySelector(".navbar");
 
-window.onscroll = () => {
-    navbar.classList.remove("active");
-};
+        menu.addEventListener("click", function () {
+            navbar.classList.toggle("active");
+            if (navbar.classList.contains("active")) {
+                menu.classList.remove("bx-menu");
+                menu.classList.add("bx-x");
+            } else {
+                menu.classList.remove("bx-x");
+                menu.classList.add("bx-menu");
+            }
+        });
+
+        // Ukryj menu przy przewijaniu
+        window.onscroll = () => {
+            navbar.classList.remove("active");
+        };
